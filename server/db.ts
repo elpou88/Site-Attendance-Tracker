39af20727a34065c737579b717b94c11adcd15ef
Jamie pou <99329078+elpou88@users.noreply.github.com>
2026-03-13 08:43:53 +0100

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,14 @@
+import { drizzle } from "drizzle-orm/node-postgres"
 import pkg from "pg"
 
 const { Pool } = pkg
 
-export const pool = new Pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-
   max: 10,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 10000,
+  connectionTimeoutMillis: 10000
 })
+
+export const db = drizzle(pool)
